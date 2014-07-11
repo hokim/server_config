11043d07f1d7408062051a6c7ca4a7633f23e7e1
hokim <hokim@kaonmedia.com>
2014-07-11 18:58:03 +0900

    Patch

--- ho_config/work/kaon/bkos200/daily-build.sh
+++ ho_config/work/kaon/bkos200/daily-build.sh
@@ -11,9 +11,6 @@ export JDK_HOME=$JAVA_HOME
 export JRE_HOME=$JAVA_HOME
 export PATH=$PATH:$JAVA_BIN
 
-# Profile
-source ~/.bashrc
-
 temp_daily_build_current_path=`pwd`
 
 # =============================================================================
@@ -24,11 +21,11 @@ temp_daily_build_current_path=`pwd`
 
 temp_daily_build_hash=`date +%Y%m%d_%H%M%S`
 
-cd /ssd2/home/$USER/archive/project/bkos200/daily-build
+cd /ssd2/home/hokim/archive/project/bkos200/daily-build
 
 echo "Start... [$temp_daily_build_hash]  `date +%Y%m%d_%H%M%S`" >> daily_build_stamp.log
 
-source $bkos200_script_path/bkos200_auto_dailybuild.sh
+source /ssd2/home/hokim/ho_config/work/kaon/bkos200/bkos200_auto_dailybuild.sh
 
 cd $temp_daily_build_current_path
 echo "Completed... [$temp_daily_build_hash]  `date +%Y%m%d_%H%M%S`" >> daily_build_stamp.log
